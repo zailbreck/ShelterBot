72cb8a910b45eb44d76b936264929b19629492a3
zailbreck <shelterxtech@gmail.com>
2022-12-10 17:17:34 +0700

    Cancel File

--- Cfg/db_config.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const sqlite3 = require('sqlite3').verbose()
-
-const db = new sqlite3.Database(path.resolve('../DB/shelterDB.db'), sqlite3.OPEN_READWRITE, (err) => {
-    if (err) throw err
-})
-
-module.exports = db
